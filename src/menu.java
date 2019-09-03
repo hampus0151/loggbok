681e70908751092139c7295cecab3dffeed49838
hampus0151 <hampus.nasvall@gmail.com>
2019-09-03 11:13:11 +0200

    workin on them menu items. added exit function

--- src/menu.java
+++ src/menu.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class menu {
     public static void main(String[] args) {
@@ -18,8 +19,29 @@ public class menu {
         System.out.println("6. Exit");
         System.out.print("Please select an option from 1-6\r\n");
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        choose();
 
+    }
+
+    private static void choose() {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        if (input == 1) {
+
+        }else if (input == 2){
+
+        }else if (input == 3){
+
+        }else if (input == 4){
+
+        }else if (input == 5){
+
+        }else if (input == 6){
+            System.exit(1);
         }
     }
+}
