0765828f09002f4547214e7a26861f4d010967f9
hampus0151 <hampus.nasvall@gmail.com>
2019-09-03 08:58:36 +0200

    first empty commit

--- src/main.java
+++ src/main.java
@@ -1,16 +1,5 @@
 import java.util.Scanner;  // Import the Scanner class
 
-class Main {
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+class main {
 
-
-
-
-
-        System.out.println("what addition to your log would you like to make?");
-
-        String add = myObj.nextLine();  // Read user input
-        System.out.println("Added text: " + add);  // Output user input
-    }
 }
