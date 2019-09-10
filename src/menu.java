3b403f17af330a452034f8f0d3b2f550c0a4a568
hampus0151 <hampus.nasvall@gmail.com>
2019-09-10 11:07:18 +0200

    commit whit menu worki

--- src/menu.java
+++ src/menu.java
@@ -2,10 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.*;
 
 public class menu {
     public static void main(String[] args) {
         menu();
+        ArrayList();
     }
 
     public static void menu() {
