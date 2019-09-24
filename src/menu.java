23c3a6c8f6ead5af8d71fd1948f8f991f835caeb
hampus0151 <hampus.nasvall@gmail.com>
2019-09-24 08:41:14 +0200

    commit whit menu workin verry good whit able to make new loggs and show old saved ones

--- src/menu.java
+++ src/menu.java
@@ -5,27 +5,33 @@ import java.util.Scanner;
 import java.util.*;
 
 public class menu {
+
+    private static ArrayList<String> minArrayLista = new ArrayList<String>();
+
     public static void main(String[] args) {
         menu();
-        ArrayList();
+
     }
 
     public static void menu() {
+        showMenu();
+
+    }
+
+    private static void showMenu(){
         System.out.println("Loggbok");
         System.out.println("Menu Options:");
         System.out.println("1. visa");
         System.out.println("2. skriv ny");
         System.out.println("3. redigera");
         System.out.println("4. spara");
-        System.out.println("5. läsa in gammal");
+        System.out.println("5. lasa in gammal");
         System.out.println("6. Exit");
         System.out.print("Please select an option from 1-6\r\n");
-
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         choose();
-
     }
+
 
     private static void choose() {
         Scanner scanner = new Scanner(System.in);
@@ -33,9 +39,9 @@ public class menu {
         int input = scanner.nextInt();
 
         if (input == 1) {
-
+            visa();
         }else if (input == 2){
-
+            skrivNy();
         }else if (input == 3){
 
         }else if (input == 4){
@@ -46,5 +52,34 @@ public class menu {
             System.exit(1);
         }
     }
+
+    private static void skrivNy(){
+        try
+        {
+            System.out.print("du valde skriv ny. Skriv din text:");
+
+            InputStreamReader in = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(in);
+            String logg = br.readLine();
+
+
+            logEntry nyLogg = new logEntry(logg);
+
+            minArrayLista.add(nyLogg.toString());
+
+            showMenu();
+        }
+        catch(Exception e){
+
+        }
+    }
+
+    private static void visa(){
+        for(String str:minArrayLista)
+            System.out.println(str);
+        showMenu();
+    }
+
+
 
 }
